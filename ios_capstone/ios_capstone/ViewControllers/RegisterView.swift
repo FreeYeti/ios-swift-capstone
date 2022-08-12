a71f1d3fa404e87a12bd9ca2ca7141bf0ae87811
krupa-patel789 <59404757+krupa-patel789@users.noreply.github.com>
2022-08-11 22:21:07 -0400

    validation

--- ios_capstone/ios_capstone/ViewControllers/RegisterView.swift
+++ ios_capstone/ios_capstone/ViewControllers/RegisterView.swift
@@ -7,19 +7,46 @@
 import UIKit
 
 class RegisterView: UIViewController {
-
+    @IBOutlet weak var
+        errorMsg:UILabel!
     @IBOutlet weak var firstNameInput: UITextField!
     @IBOutlet weak var lastNameInput: UITextField!
     @IBOutlet weak var emailInput: UITextField!
     @IBOutlet weak var passwordInput: UITextField!
+    
+    	
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.navigationItem.title = "SignUp"
     }
     
+    func checkAndDisplayError()->Bool{
+        let fname = self.firstNameInput?.text ?? ""
+        let lname = self.lastNameInput?.text ?? ""
+        let email = self.emailInput?.text ?? ""
+        let password = self.passwordInput?.text ?? ""
+        
+        if !fname.isEmpty && !lname.isEmpty && !email.isEmpty && !password.isEmpty{
+            if(password.count >= 8){
+                return true
+            }else{
+                errorMsg.text = "length must be 8 of the password"
+
+return false
+            }
+        }else{
+            
+            errorMsg.text = "Plese fill the all fields"
+          return false
+        }
+    }
 
     @IBAction func register(_ sender: Any) {
+        
+        
+        if(checkAndDisplayError() == false){return}
+        
         let json: [String: Any] = ["email": emailInput.text!,
                                    "password": passwordInput.text!,
                                    "first_name": firstNameInput.text!,
@@ -36,7 +63,7 @@ class RegisterView: UIViewController {
         // insert json data to the request
         request.httpBody = jsonData
 
-        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+        let task = URLSession.shared.dataTask(with: request) { [self] data, response, error in
             guard let data = data, error == nil else {
                 print(error?.localizedDescription ?? "No data")
                 return
@@ -44,8 +71,11 @@ class RegisterView: UIViewController {
             let responseJSON = try? JSONSerialization.jsonObject(with: data, options: [])
             if let responseJSON = responseJSON as? [String: Any] {
                 
+                errorMsg.text = "Account not created"
+                
                 print(responseJSON)
             }
+            errorMsg.text = "Account has been created successfully"
             print(responseJSON ?? "No JSON data to show you! :(")
         }
         
